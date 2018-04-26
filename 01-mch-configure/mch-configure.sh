37fed850744877aa5be8682d5edcca9e07993c84
Lucas Russo <lerwys@gmail.com>
2018-04-26 19:57:09 -0300

    01-mch-configure/mch-configure.sh: use only the Crate number as option
    The hostnames will be fetched from the
    crate-fpga-mapping.sh file

--- 01-mch-configure/mch-configure.sh
+++ 01-mch-configure/mch-configure.sh
@@ -9,9 +9,41 @@ SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
 # Source common functions
 . ${SCRIPTPATH}/../misc/functions.sh
+# Source Crate FPGA mapping
+. ${SCRIPTPATH}/../misc/crate-fpga-mapping.sh
+
+set +u
+
+# Simple argument checking
+if [ $# -lt 3 ]; then
+    echo "Usage: ./mch-configure.sh <Connection Type> [<MCH IP> <MCH Password>] <FTP Server Address> <Crate Number>"
+    exit 1
+fi
 
 # MCH config arguments
-MCH_CONFIG_ARGS="$@"
+MCH_CONN_TYPE="$1"
+
+if [ ${MCH_CONN_TYPE} == "ssh" ]; then
+    MCH_IPADDR="$2"
+    MCH_PASS="$3"
+    TFTP_IPADDR="$4"
+    CRATE_NUMBER_="$5"
+elif [ ${MCH_CONN_TYPE} == "serial" ]; then
+    TFTP_IPADDR="$2"
+    CRATE_NUMBER_="$3"
+else
+    exec_cmd "ERR  " echo "Invalid MCH Connection Type. Valid options are \"ssh\" or \"serial\""
+    exit 1
+fi
+
+# Remove leading zeros
+CRATE_NUMBER="$(echo ${CRATE_NUMBER_} | sed 's/^0*//')"
+
+set -u
+
+# Get MCH Hostname
+MCH_HOSTNAME_="BPM_CRATE_${CRATE_NUMBER}_MCH_HOSTNAME"
+MCH_HOSTNAME="${!MCH_HOSTNAME_}"
 
 MCH_FIRMWARES_DIR_REL=firmwares
 MCH_FIRMWARES_DIR=${SCRIPTPATH}/${MCH_FIRMWARES_DIR_REL}
@@ -42,5 +74,15 @@ ${SCRIPTPATH}/serial-config.sh
 
 exec_cmd "INFO " echo "Configuring MCH..."
 
-# Call actual MCH config script
-${SCRIPTPATH}/mch-config.exp ${MCH_CONFIG_ARGS}
+
+if [ ${MCH_CONN_TYPE} == "ssh" ]; then
+    MCH_IPADDR="$2"
+    MCH_PASS="$3"
+    TFTP_IPADDR="$4"
+    CRATE_NUMBER="$5"
+    # Call actual MCH config script
+    ${SCRIPTPATH}/mch-config.exp ${MCH_CONN_TYPE} ${MCH_IPADDR} ${MCH_PASS} ${TFTP_IPADDR} ${MCH_HOSTNAME}
+elif [ ${MCH_CONN_TYPE} == "serial" ]; then
+    # Call actual MCH config script
+    ${SCRIPTPATH}/mch-config.exp ${MCH_CONN_TYPE} ${TFTP_IPADDR} ${MCH_HOSTNAME}
+fi
