9f09b49e368cbd1edc9beb3cdc607b8cc4767f71
Lucas Russo <lerwys@gmail.com>
2018-05-11 15:34:48 -0300

    01-mch-configure/*: split package check/install

--- 01-mch-configure/serial-config.sh
+++ 01-mch-configure/serial-config.sh
@@ -4,22 +4,46 @@ set -euo pipefail
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
-# Source RFFE mapping IPs
+# Source misc functions
 . ${SCRIPTPATH}/../misc/functions.sh
 
 # Check if we already have the packages
 if ! [[ $(which cu) ]] || ! [[ $(which expect) ]] || \
-    ! [[ $(which tftp) ]]; then
+    ! [[ $(which tftp) ]] || ! [ -d /var/lib/tftpboot ]; then
+    UPDATE_CMD="apt-get update"
+fi
 
-    exec_cmd "TRACE" echo "Installing needed packages..."
-    sudo apt-get update && \
-    sudo apt-get install -y \
-        cu \
-        expect \
-        tftpd-hpa \
-        tftp
+PACKS=()
+INSTALL_CMD="apt-get install -y"
+# Install each one of the missing ones
+if ! [[ $(which cu) ]] ; then
+    PACKS+=("cu")
+fi
+
+if ! [[ $(which expect) ]] ; then
+    PACKS+=("expect")
+fi
+
+if ! [[ $(which tftp) ]] ; then
+    PACKS+=("tftp")
+fi
+
+if ! [ -d /var/lib/tftpboot ]; then
+    PACKS+=("tftpd-hpa")
+fi
+
+# Update cache if needed
+if [ -v UPDATE_CMD ]; then
+    exec_cmd "TRACE" echo "Updating cache..."
+    sudo ${UPDATE_CMD}
     exec_cmd "TRACE" echo "Success!"
+fi
 
+# Install packages if needed
+if [ -v PACKS ]; then
+    exec_cmd "TRACE" echo "Installing needed packages..."
+    sudo ${INSTALL_CMD} "${PACKS[@]}"
+    exec_cmd "TRACE" echo "Success!"
 fi
 
 exec_cmd "TRACE" echo "Adding your user ($USER) to the dialout group (so cu can access the serial ports)..."
