e91d931df3ad40c6b26ce042b4d34d4bb28fc314
Lucas Russo <lerwys@gmail.com>
2018-05-15 20:18:06 -0300

    ethswitch-configure.sh: remove unneeded arguments fro TFTP

--- 00-ethswitch-configure/ethswitch-configure.sh
+++ 00-ethswitch-configure/ethswitch-configure.sh
@@ -7,18 +7,6 @@ SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 # Source common functions
 . ${SCRIPTPATH}/../misc/functions.sh
 
-set +u
-
-# Simple argument checking
-if [ $# -lt 1 ]; then
-    echo "Usage: ./ethswitch-configure.sh <FTP Server Address>"
-    exit 1
-fi
-
-TFTP_IPADDR="$1"
-
-set -u
-
 exec_cmd "INFO " echo "Setting up Serial connection..."
 
 # Call actual serial config
@@ -27,4 +15,4 @@ ${SCRIPTPATH}/serial-config.sh
 exec_cmd "INFO " echo "Configuring Switch..."
 
 # Call actual switch config script
-${SCRIPTPATH}/ethswitch-config.exp ${TFTP_IPADDR}
+${SCRIPTPATH}/ethswitch-config.exp
