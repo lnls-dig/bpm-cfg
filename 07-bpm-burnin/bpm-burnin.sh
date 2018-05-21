056aa68c5a6da233a26882bc507aa24931b2e4a6
Daniel Tavares <daniel.tavares@lnls.br>
2018-05-21 11:07:43 -0300

    07-bpm-burnin: add script to perform burn-in tests

---
+++ 07-bpm-burnin/bpm-burnin.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+
+# Source RFFE mapping IPs
+
+set +u
+
+# Simple argument checking
+if [ $# -lt 2 ]; then
+    echo "Usage: ./bpm-burnin.sh <Crate Number> <Destination folder for test results>"
+    exit 1
+fi
+
+CRATE_NUMBER_="$1"
+# Remove leading zeros
+CRATE_NUMBER="$(echo ${CRATE_NUMBER_} | sed 's/^0*//')"
+TEST_RESULT_FOLDER="$2"
+
+set -u
+
+# Check if repo was cloned with fpga-programming submodule
+[ ! -z "$(ls -A "${SCRIPTPATH}/../foreign/bpm-tests")" ] || \
+    git submodule update --init
+
+${SCRIPTPATH}/../foreign/bpm-tests/site_specific/sirius/tests/test_burnin/test_burnin.m ${CRATE_NUMBER} ${TEST_RESULT_FOLDER}
