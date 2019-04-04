fe4a51408b7fdda9c96fe54cea40f15234d9f832
Lucas Russo <lerwys@gmail.com>
2019-04-04 14:27:45 -0300

    04-fpga-configure: update bitstream version to 2.3.0

--- 04-fpga-configure/fpga-bitstreams.sh
+++ 04-fpga-configure/fpga-bitstreams.sh
@@ -4,12 +4,12 @@
 # BPM
 ############################
 BPM_URL_RELEASE_BASE="https://github.com/lnls-dig/bpm-gw/releases/download"
-BPM_TAG_VERSION="v1.1.1"
+BPM_TAG_VERSION="v2.3.0"
 
 # Bitstream name without the extension!
-FPGA_BO_BITSTREAM="afcv3-bpm-gw-fmc250m-bo-sirius-v1.1.1-20180302-91ebf0c889"
-FPGA_SR_BITSTREAM="afcv3-bpm-gw-fmc250m-sr-sirius-v1.1.1-20180302-91ebf0c889"
-FPGA_PBPM_BITSTREAM="afcv3-pbpm-gw-fmcpico1m-v1.1.1-20180302-91ebf0c889"
+FPGA_BO_BITSTREAM="afcv3-bpm-gw-fmc250m-bo-sirius-v2.3.0-20190404-f6e88ce5be"
+FPGA_SR_BITSTREAM="afcv3-bpm-gw-fmc250m-sr-sirius-v2.3.0-20190404-f6e88ce5be"
+FPGA_PBPM_BITSTREAM="afcv3-pbpm-gw-fmcpico1m-v2.3.0-20190404-f6e88ce5be"
 
 URL_FPGA_BO_BITSTREAM=${BPM_URL_RELEASE_BASE}/${BPM_TAG_VERSION}/${FPGA_BO_BITSTREAM}
 URL_FPGA_SR_BITSTREAM=${BPM_URL_RELEASE_BASE}/${BPM_TAG_VERSION}/${FPGA_SR_BITSTREAM}
