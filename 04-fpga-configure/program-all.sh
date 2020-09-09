b8f84628184edd52e90b75c10715400040700d5f
Lucas Russo <lerwys@gmail.com>
2020-09-09 13:18:59 -0300

    04-fpga-configure: fix wrong crate numbers

--- 04-fpga-configure/program-all.sh
+++ 04-fpga-configure/program-all.sh
@@ -2,7 +2,7 @@
 
 set -auxo pipefail
 
-for i in `seq -f %02g 2 20`; do
+for i in `seq -f %02g 1 20`; do
 
     crate_num=$(echo ${i} | sed 's/^0*//')
     crate_name=$(echo IA-${i}RaBPM-CO-CrateCtrl)
@@ -14,7 +14,7 @@ done
 for i in `seq -f %02g 21 21`; do
 
     crate_num=$(echo ${i} | sed 's/^0*//')
-    crate_name=$(echo IA-RaBPMTL-CO-CrateCtrl)
+    crate_name=$(echo IA-20RaBPMTL-CO-CrateCtrl)
     crate_date=$(date  +%Y%m%d-%H%M%S)
     ./fpga-configure.sh ${crate_name} ${crate_num} &
 
