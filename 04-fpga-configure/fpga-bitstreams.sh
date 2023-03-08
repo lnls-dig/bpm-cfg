85a156e0240028cb6f56668d13e34a410491822a
guilherme.ricioli <guilherme.riciolic@gmail.com>
2023-03-08 14:50:22 -0300

    program-bpms.sh: remove unused variables
    This script doesn't need to know about crate number or MCH's XVC port.
    Also, the bitstream argument is now expected to have the .nvsf
    extension.

--- 04-fpga-configure/fpga-bitstreams.sh
+++ 04-fpga-configure/fpga-bitstreams.sh
@@ -6,10 +6,10 @@
 BPM_URL_RELEASE_BASE="https://github.com/lnls-dig/bpm-gw/releases/download"
 BPM_TAG_VERSION="v2.3.0"
 
-# Bitstream name without the extension!
-FPGA_BO_BITSTREAM="afcv3-bpm-gw-fmc250m-bo-sirius-v2.3.0-20190404-f6e88ce5be"
-FPGA_SR_BITSTREAM="afcv3-bpm-gw-fmc250m-sr-sirius-v2.3.0-20190404-f6e88ce5be"
-FPGA_PBPM_BITSTREAM="afcv3-pbpm-gw-fmcpico1m-v2.3.0-20190404-f6e88ce5be"
+# NSVF files
+FPGA_BO_BITSTREAM="afcv3-bpm-gw-fmc250m-bo-sirius-v2.3.0-20190404-f6e88ce5be.nvsf"
+FPGA_SR_BITSTREAM="afcv3-bpm-gw-fmc250m-sr-sirius-v2.3.0-20190404-f6e88ce5be.nvsf"
+FPGA_PBPM_BITSTREAM="afcv3-pbpm-gw-fmcpico1m-v2.3.0-20190404-f6e88ce5be.nsvf"
 
 URL_FPGA_BO_BITSTREAM=${BPM_URL_RELEASE_BASE}/${BPM_TAG_VERSION}/${FPGA_BO_BITSTREAM}
 URL_FPGA_SR_BITSTREAM=${BPM_URL_RELEASE_BASE}/${BPM_TAG_VERSION}/${FPGA_SR_BITSTREAM}
@@ -21,8 +21,8 @@ URL_FPGA_PBPM_BITSTREAM=${BPM_URL_RELEASE_BASE}/${BPM_TAG_VERSION}/${FPGA_PBPM_B
 FOFB_URL_RELEASE_BASE=""
 FOFB_TAG_VERSION=""
 
-# Bitstream name without the extension!
-FPGA_FOFB_BITSTREAM="afcv4_ref_fofb_ctrl"
+# NSVF file
+FPGA_FOFB_BITSTREAM="afcv4_ref_fofb_ctrl.nsvf"
 
 #URL_FPGA_FOFB_BITSTREAM=${FOFB_URL_RELEASE_BASE}/${FOFB_TAG_VERSION}/${FPGA_FOFB_BITSTREAM}
 # Keep this empty until we have the fofb bitstream available
@@ -34,8 +34,8 @@ URL_FPGA_FOFB_BITSTREAM=""
 TIMING_URL_RELEASE_BASE=""
 TIMING_TAG_VERSION=""
 
-# Bitstream name without the extension!
-FPGA_TIMING_BITSTREAM="afcv3-timing-vx.y.z-X-Y"
+# NSVF file
+FPGA_TIMING_BITSTREAM="afcv3-timing-vx.y.z-X-Y.nsvf"
 
 #URL_FPGA_TIMING_BITSTREAM=${TIMING_URL_RELEASE_BASE}/${TIMING_TAG_VERSION}/${FPGA_TIMING_BITSTREAM}
 # Keep this empty until we have the timing bitstream available
