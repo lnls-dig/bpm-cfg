25403742062ec94668b66b2b1e390489c059bf57
guilherme.ricioli <guilherme.riciolic@gmail.com>
2023-03-08 15:12:33 -0300

    fpga-configure.sh: some clean up
    Remove support for downloading bitstreams from releases. This is done so
    to simplify the deploy scripts.
    
    Also, fixed identation.

--- 04-fpga-configure/fpga-bitstreams.sh
+++ 04-fpga-configure/fpga-bitstreams.sh
@@ -3,53 +3,26 @@
 ############################
 # BPM
 ############################
-BPM_URL_RELEASE_BASE="https://github.com/lnls-dig/bpm-gw/releases/download"
-BPM_TAG_VERSION="v2.3.0"
-
 # NSVF files
-FPGA_BO_BITSTREAM="afcv3-bpm-gw-fmc250m-bo-sirius-v2.3.0-20190404-f6e88ce5be.nvsf"
-FPGA_SR_BITSTREAM="afcv3-bpm-gw-fmc250m-sr-sirius-v2.3.0-20190404-f6e88ce5be.nvsf"
-FPGA_PBPM_BITSTREAM="afcv3-pbpm-gw-fmcpico1m-v2.3.0-20190404-f6e88ce5be.nsvf"
-
-URL_FPGA_BO_BITSTREAM=${BPM_URL_RELEASE_BASE}/${BPM_TAG_VERSION}/${FPGA_BO_BITSTREAM}
-URL_FPGA_SR_BITSTREAM=${BPM_URL_RELEASE_BASE}/${BPM_TAG_VERSION}/${FPGA_SR_BITSTREAM}
-URL_FPGA_PBPM_BITSTREAM=${BPM_URL_RELEASE_BASE}/${BPM_TAG_VERSION}/${FPGA_PBPM_BITSTREAM}
+FPGA_BO_BITSTREAM=""
+FPGA_SR_BITSTREAM=""
+FPGA_PBPM_BITSTREAM=""
 
 ############################
 # FOFB
 ############################
-FOFB_URL_RELEASE_BASE=""
-FOFB_TAG_VERSION=""
-
 # NSVF file
-FPGA_FOFB_BITSTREAM="afcv4_ref_fofb_ctrl.nsvf"
-
-#URL_FPGA_FOFB_BITSTREAM=${FOFB_URL_RELEASE_BASE}/${FOFB_TAG_VERSION}/${FPGA_FOFB_BITSTREAM}
-# Keep this empty until we have the fofb bitstream available
-URL_FPGA_FOFB_BITSTREAM=""
+FPGA_FOFB_BITSTREAM=""
 
 ############################
 # Timing
 ############################
-TIMING_URL_RELEASE_BASE=""
-TIMING_TAG_VERSION=""
-
 # NSVF file
-FPGA_TIMING_BITSTREAM="afcv3-timing-vx.y.z-X-Y.nsvf"
-
-#URL_FPGA_TIMING_BITSTREAM=${TIMING_URL_RELEASE_BASE}/${TIMING_TAG_VERSION}/${FPGA_TIMING_BITSTREAM}
-# Keep this empty until we have the timing bitstream available
-URL_FPGA_TIMING_BITSTREAM=""
+FPGA_TIMING_BITSTREAM=""
 
 ############################
 # All FPGA Bitstreams
 ############################
-# Keep them synched!
-URL_FPGA_ALL=(${URL_FPGA_BO_BITSTREAM}
-            ${URL_FPGA_SR_BITSTREAM}
-            ${URL_FPGA_PBPM_BITSTREAM}
-            ${URL_FPGA_FOFB_BITSTREAM}
-            ${URL_FPGA_TIMING_BITSTREAM})
 FPGA_BITSTREAMS_ALL=(${FPGA_BO_BITSTREAM}
                     ${FPGA_SR_BITSTREAM}
                     ${FPGA_PBPM_BITSTREAM}
--- 04-fpga-configure/fpga-configure.sh
+++ 04-fpga-configure/fpga-configure.sh
@@ -36,74 +36,53 @@ set -u
 [ ! -z "$(ls -A "${SCRIPTPATH}/../foreign/fpga-programming")" ] || \
     git submodule update --init
 
-## Ask sudo password only once and
-## keep updating sudo timestamp to
-## avoid asking again
-#sudo -v
-#while true; do sudo -n true; sleep 60; kill -0 "$$" || \
-#    exit; done 2>/dev/null &
-#
-### Install packages
-#sudo apt-get update && \
-#sudo apt-get install -y \
-#    wget
+exec_cmd "INFO " echo "Checking bitstream files..."
 
-exec_cmd "INFO " echo "Checking nsvf files..."
+for bit in "${FPGA_BITSTREAMS_ALL[@]}"; do
 
-NSVF_EXTENSION=.nsvf
-# Download bitstreams
-mkdir -p ${FPGA_BITSTREAMS_DIR}
-for i in `seq 0 $((${#URL_FPGA_ALL[@]}-1))`; do
-    if [ ! -z ${URL_FPGA_ALL[$i]} ]; then
-        bash -c "\
-            SCRIPTPATH=\"\$( cd \"\$( dirname ${BASH_SOURCE[0]}  )\" && pwd  )\" && \
-            . \${SCRIPTPATH}/../misc/functions.sh && \
-            cd ${FPGA_BITSTREAMS_DIR}; \
-            if [ ! -f ${FPGA_BITSTREAMS_ALL[$i]}${NSVF_EXTENSION} ]; then
-                exec_cmd \"ERR  \" echo \"File \
-                    ${FPGA_BITSTREAMS_ALL[$i]}${NSVF_EXTENSION} not found.\" >&2
-                exit 1
-            fi \
-        "
-    fi
+  if [ ! -f ${FPGA_BITSTREAMS_DIR}/${bit} ]; then
+    exec_cmd "ERR  " echo "File ${bit} not found." >&2
+    exit 1
+  fi
+
 done
 
-
-exec_cmd "INFO " echo "Flashing FPGA Gateware of all boards..."
+exec_cmd "INFO " echo "Flashing FPGA gateware of all boards..."
 
 BPM_MAX_NUM_BOARDS=12
-for i in `seq 1 ${BPM_MAX_NUM_BOARDS}`; do
-    bpm_crate_fpga_raw="BPM_CRATE_${CRATE_NUMBER}_FPGA[${i}]"
-    bpm_crate_fpga=${!bpm_crate_fpga_raw}
+for slot in `seq 1 ${BPM_MAX_NUM_BOARDS}`; do
 
-    # Get real FPGA bitstream name
-    case ${bpm_crate_fpga} in
-        "timing")
-            board_fpga=${FPGA_BITSTREAMS_DIR}/${FPGA_TIMING_BITSTREAM}
-        ;;
-        "fofb")
-            board_fpga=${FPGA_BITSTREAMS_DIR}/${FPGA_FOFB_BITSTREAM}
-        ;;
-        "pbpm")
-            board_fpga=${FPGA_BITSTREAMS_DIR}/${FPGA_PBPM_BITSTREAM}
-        ;;
-        "sr")
-            board_fpga=${FPGA_BITSTREAMS_DIR}/${FPGA_SR_BITSTREAM}
-        ;;
-        "bo")
-            board_fpga=${FPGA_BITSTREAMS_DIR}/${FPGA_BO_BITSTREAM}
-        ;;
-        "")
-            # Skip it
-            continue
-        ;;
-        *)
-            exec_cmd "ERR  " echo "Invalid FPGA bitstream type: ${bpm_crate_fpga}" >&2
-            exit 1
-        ;;
-    esac
+  bpm_crate_fpga_raw="BPM_CRATE_${CRATE_NUMBER}_FPGA[${slot}]"
+  bpm_crate_fpga=${!bpm_crate_fpga_raw}
 
-    ${SCRIPTPATH}/program-bpms.sh ${MCH_IP} "${board_fpga},$((${slot} - 1))" | \
-        tee -a log_bpm_${CRATE_NUMBER}.log 2>&1 || \
-        true
+  # Get real FPGA bitstream name
+  case ${bpm_crate_fpga} in
+    "timing")
+      board_fpga=${FPGA_BITSTREAMS_DIR}/${FPGA_TIMING_BITSTREAM}
+    ;;
+    "fofb")
+      board_fpga=${FPGA_BITSTREAMS_DIR}/${FPGA_FOFB_BITSTREAM}
+    ;;
+    "pbpm")
+      board_fpga=${FPGA_BITSTREAMS_DIR}/${FPGA_PBPM_BITSTREAM}
+    ;;
+    "sr")
+      board_fpga=${FPGA_BITSTREAMS_DIR}/${FPGA_SR_BITSTREAM}
+    ;;
+    "bo")
+      board_fpga=${FPGA_BITSTREAMS_DIR}/${FPGA_BO_BITSTREAM}
+    ;;
+    "")
+      # Skip it
+      continue
+    ;;
+    *)
+      exec_cmd "ERR  " echo "Invalid bitstream type: ${bpm_crate_fpga}" >&2
+      exit 1
+    ;;
+  esac
+
+  ${SCRIPTPATH}/program-bpms.sh ${MCH_IP} "${board_fpga},$((${slot} - 1))" | \
+    tee -a log_bpm_${CRATE_NUMBER}.log 2>&1 || \
+    true
 done
