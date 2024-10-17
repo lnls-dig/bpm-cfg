29f7f79fbd73c16f8c6ba983e5700a8639ff68e8
Gustavo de Souza dos Reis <gustavo.reis@lnls.br>
2024-10-17 09:57:45 -0300

    Add script to reset rffe.

---
+++ 02-cpu-configure/rffe-reset.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eu
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <virtual_slot>"
+    exit 1
+fi
+
+expect << EOF
+spawn telnet 192.168.2.$(( 190 + $1 ))
+expect "NuttShell (NSH)"
+send "reboot\r"
+EOF
