3522e8d4c849f43a496bd831dddde08083616d12
Gustavo de Souza dos Reis <gustavo.reis@lnls.br>
2024-11-08 15:44:25 -0300

    Refactor set-fru-info.py script to not hardcode FRU infos.

--- 03-mmc-upload/update-fru.sh
+++ 03-mmc-upload/update-fru.sh
@@ -4,9 +4,6 @@ set -euo pipefail
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
-# Source AFCv4 mapping
-CSV_PATH=${SCRIPTPATH}/../misc/AFCv4List.csv
-
 # Source FPGA mapping
 . ${SCRIPTPATH}/../misc/crate-fpga-mapping.sh
 
@@ -17,12 +14,15 @@ mkdir -p ${FRU_BINARIES_DIR}
 set +u
 
 # Simple argument checking
-if [ $# -lt 1 ]; then
-    echo "Usage: ./update-fru.sh <Crate Number>"
+if [ $# -lt 2 ]; then
+    echo "Usage: ./update-fru.sh <Crate Number> [arguments for set-fru-info.py]"
     exit 1
 fi
 
 CRATE_NUMBER="$1"
+shift
+FRU_ARGS="$@"
+
 crate_ip_raw="BPM_CRATE_${CRATE_NUMBER}_MCH_HOSTNAME"
 crate_ip=${!crate_ip_raw}
 
@@ -34,7 +34,8 @@ if [[ $? != 0 ]]; then
     exit 1
 fi
 
-./set-fru-info.py --csv-in ${CSV_PATH} --rack_number ${CRATE_NUMBER} --bin-in ${FRU_BINARIES_DIR}/rack-${CRATE_NUMBER}-read.bin --bin-out ${FRU_BINARIES_DIR}/rack-${CRATE_NUMBER}-write.bin
+# Pass additional arguments to the Python script
+./set-fru-info.py --bin-in ${FRU_BINARIES_DIR}/rack-${CRATE_NUMBER}-read.bin --bin-out ${FRU_BINARIES_DIR}/rack-${CRATE_NUMBER}-write.bin ${FRU_ARGS}
 
 ipmitool -I lan -H ${crate_ip} -A none -T 0x82 -m 0x20 -t 116 fru write 0 ${FRU_BINARIES_DIR}/rack-${CRATE_NUMBER}-write.bin
 if [[ $? != 0 ]]; then
